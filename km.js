29e813673336dcf4b258009c64de298b03587671
karan mainali <karanmainali123@gmail.com>
2023-11-02 17:21:09 +0530

    commit

--- km.js
+++ km.js
@@ -1,27 +1,28 @@
 const http=require('http');
 
-const server=http.createServere(callBackFunction);
+const server=http.createServer(callBackFunction);
 
 
 let a=[];
 function callBackFunction(req,res){
     if(req.method == 'post'){
         a.push(1);
-        res.write(JSON.stringify(a));
+        
     }
     else if (req.method == 'PUT'){
         a[0] = 2;
-        res.write(JSON.stringify(a));
+        
         
     }else if (req.method == 'GET'){
-        res.write(JSON.stringify(a));
+        
     }
     else if (req.method=='DELETE'){
     a.pop();
     
 }
+res.write(JSON.stringify(a));
 res.end();
 }
-server.listen(5000,function(){
+server.listen(5006,function(){
     console.log('server started');
 });
